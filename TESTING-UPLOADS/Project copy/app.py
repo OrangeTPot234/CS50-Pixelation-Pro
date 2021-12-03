202cac317ae42c02fd04d3e64e49a7f6843f5a3e
OrangeTPot234 <90113178+OrangeTPot234@users.noreply.github.com>
2021-12-03 02:21:45 +0000

    Fri, Dec 3, 2021, 2:21 AM +00:00

--- TESTING-UPLOADS/Project copy/app.py
+++ TESTING-UPLOADS/Project copy/app.py
@@ -4,7 +4,7 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
-from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
