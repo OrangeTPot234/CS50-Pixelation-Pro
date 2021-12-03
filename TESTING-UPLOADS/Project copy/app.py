d98fd914af0274630f2ea6d24e1ffe4643a35ff2
OrangeTPot234 <90113178+OrangeTPot234@users.noreply.github.com>
2021-12-03 02:07:32 +0000

    Fri, Dec 3, 2021, 2:07 AM +00:00

--- TESTING-UPLOADS/Project copy/app.py
+++ TESTING-UPLOADS/Project copy/app.py
@@ -9,12 +9,12 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required
 
-def insert_picture(conn, picture_file):
+"""def insert_picture(conn, picture_file):
     with open(picture_file, 'rb') as input_file:
         ablob = input_file.read()
         base=os.path.basename(picture_file)
         afile, ext = os.path.splitext(base)
-        db.execute("")
+        db.execute("")"""
 
 # Configure application
 app = Flask(__name__)
