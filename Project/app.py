c7d9481b4c8a61ec70adf019d326af72798faccd
OrangeTPot234 <90113178+OrangeTPot234@users.noreply.github.com>
2021-12-04 20:48:16 +0000

    Sat, Dec 4, 2021, 8:48 PM +00:00

--- Project/app.py
+++ Project/app.py
@@ -5,6 +5,7 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.utils import secure_filename
 
 from helpers import apology, login_required, insert_picture
 
