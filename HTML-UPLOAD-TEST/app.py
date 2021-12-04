0facb09ab46bf8eb5bae83337a2ea2b9033717d0
OrangeTPot234 <90113178+OrangeTPot234@users.noreply.github.com>
2021-12-04 21:47:49 +0000

    Sat, Dec 4, 2021, 9:47 PM +00:00

--- HTML-UPLOAD-TEST/app.py
+++ HTML-UPLOAD-TEST/app.py
@@ -3,12 +3,25 @@ from werkzeug.utils import secure_filename
 from cs50 import SQL
 
 app = Flask(__name__)
-db = SQL("sqlite:///databases.db")
+db = SQL("sqlite:///phototest.db")
 
 def insert_picture(picture_file):
     with open(picture_file, 'rb') as input_file:
         blob = input_file.read()
         db.execute("INSERT INTO photos (gallery_id, photo_name, photo_file) VALUES (?, ?, ?)", 1, "photo", blob)
+
+def extract_picture(picture_id):
+    db.execute("SELECT photo, photo_name FROM photos WHERE photo_id = ?", picture_id)
+    
+    sql = "SELECT PICTURE, TYPE, FILE_NAME FROM PICTURES WHERE id = :id"
+    param = {'id': picture_id}
+    cursor.execute(sql, param)
+    ablob, ext, afile = cursor.fetchone()
+    filename = afile + ext
+    with open(filename, 'wb') as output_file:
+        output_file.write(ablob)
+    return filename
+
 
 @app.route('/')
 def form():
