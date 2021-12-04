954c3f80b380faf5b11083aa2677c97d8f284c5e
OrangeTPot234 <90113178+OrangeTPot234@users.noreply.github.com>
2021-12-04 19:35:21 +0000

    Sat, Dec 4, 2021, 7:35 PM +00:00

--- HTML-UPLOAD-TEST/app.py
+++ HTML-UPLOAD-TEST/app.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template,request
 from werkzeug.utils import secure_filename
  
+ app = Flask(__name__)
+
 @app.route('/form')
 def form():
     return render_template('form.html')
