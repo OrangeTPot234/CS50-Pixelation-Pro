04da9ccfac1a181c70c4cc803b50dd4ee0b01f33
OrangeTPot234 <90113178+OrangeTPot234@users.noreply.github.com>
2021-12-04 19:49:59 +0000

    Sat, Dec 4, 2021, 7:49 PM +00:00

--- HTML-UPLOAD-TEST/app.py
+++ HTML-UPLOAD-TEST/app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,request
 from werkzeug.utils import secure_filename
  
 app = Flask(__name__)
-app.config[‘UPLOAD_FOLDER‘] = 
+app.config['UPLOAD_FOLDER'] = "/Files"
 
 @app.route('/')
 def form():
@@ -13,9 +13,10 @@ def form():
 def upload():
     if request.method == 'POST':
         screenload = 1
+        print("If statement worked")
         f = request.files['file']
         f.save(secure_filename(f.filename))
-        print("")
+        print("f.save worked")
         return render_template('form.html', screenload=screenload)
  
 app.run(host='localhost', port=5000)
