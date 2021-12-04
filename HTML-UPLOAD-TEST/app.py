654d2682383f14ce9ebd044b46b2705bf299a5bf
OrangeTPot234 <90113178+OrangeTPot234@users.noreply.github.com>
2021-12-04 19:42:27 +0000

    Sat, Dec 4, 2021, 7:42 PM +00:00

--- HTML-UPLOAD-TEST/app.py
+++ HTML-UPLOAD-TEST/app.py
@@ -5,13 +5,15 @@ app = Flask(__name__)
 
 @app.route('/form')
 def form():
-    return render_template('form.html')
+    screenload = 0
+    return render_template('form.html', screenload=screenload)
  
 @app.route('/upload', methods = ['POST', 'GET'])
 def upload():
     if request.method == 'POST':
+        screenload = 1
         f = request.files['file']
         f.save(secure_filename(f.filename))
-        return "File saved successfully"
+        return render_template('form.html', screenload=screenload)
  
 app.run(host='localhost', port=5000)
