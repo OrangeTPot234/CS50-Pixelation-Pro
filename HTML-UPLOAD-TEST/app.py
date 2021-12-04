9e6b94c58215a808d35ee2e4201c698f7742f28c
OrangeTPot234 <90113178+OrangeTPot234@users.noreply.github.com>
2021-12-04 20:14:42 +0000

    Sat, Dec 4, 2021, 8:14 PM +00:00

--- HTML-UPLOAD-TEST/app.py
+++ HTML-UPLOAD-TEST/app.py
@@ -1,7 +1,6 @@
 from flask import Flask,render_template,request
 from werkzeug.utils import secure_filename
 from cs50 import SQL
-import sqlite3
 
 app = Flask(__name__)
 db = SQL("sqlite:///databases.db")
@@ -9,13 +8,7 @@ db = SQL("sqlite:///databases.db")
 def insert_picture(picture_file):
     with open(picture_file, 'rb') as input_file:
         blob = input_file.read()
-        #base=os.path.basename(picture_file)
-        #afile, ext = os.path.splitext(base)
-        #sql = '''INSERT INTO PICTURES
-        #(PICTURE, TYPE, FILE_NAME)
-        #VALUES(?, ?, ?);'''
         db.execute("INSERT INTO photos (gallery_id, photo_name, photo_file) VALUES (?, ?, ?)", 1, "photo", blob)
-        #db.execute(sql,[sqlite3.Binary(ablob), ext, afile]) 
 
 @app.route('/')
 def form():
