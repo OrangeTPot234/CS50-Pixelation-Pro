4eab00ac71e427656e6a76a21dbb7fb325cccb8a
OrangeTPot234 <90113178+OrangeTPot234@users.noreply.github.com>
2021-12-05 18:58:07 +0000

    Sun, Dec 5, 2021, 6:58 PM +00:00

--- HTML-UPLOAD-TEST/app.py
+++ HTML-UPLOAD-TEST/app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,request, url_for
 from werkzeug.utils import secure_filename
 from cs50 import SQL
+import os
 
 app = Flask(__name__)
 db = SQL("sqlite:///phototest.db")
@@ -32,12 +33,17 @@ def upload():
     if request.method == 'POST':
         screenload = 1
         print("If statement worked")
+        # pull from upload button
         f = request.files['file']
         f.save(secure_filename(f.filename))
         print("f.save worked")
+        # insert into database
         insert_picture(f.filename.replace(" ", "_"))
+        os.remove(f.filename)
         print("Insert Picture Worked")
+        #extract photo
         picture = extract_picture(1)
+        # print photo
         g = picture
         return render_template('form.html', screenload=screenload, picture=g)
  
