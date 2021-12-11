ba95f021aac36295fd136623b381957d840d08d3
OrangeTPot234 <90113178+OrangeTPot234@users.noreply.github.com>
2021-12-11 05:09:07 +0000

    Sat, Dec 11, 2021, 5:09 AM +00:00

--- Project/app.py
+++ Project/app.py
@@ -203,7 +203,7 @@ def gallery():
         counter = gallery_info[0]["views"] + 1
         db.execute("UPDATE galleries SET views = ? WHERE gallery_id = ?", counter, gallery_id) 
         photos = extract_pictures(gallery_id, "gal")
-        comments = db.execute("SELECT * FROM comments WHERE gallery_id = ?",gallery_id)
+        comments = db.execute("SELECT * FROM comments WHERE gallery_id = ? ORDER BY timestamp ASC",gallery_id)
         return render_template("gallery.html", gallery_name=gallery_info[0]['gallery_name'], photo_list=photos, comments=comments)
 
 @app.route("/search", methods=["GET", "POST"])
